5e5c99874a3ed999d39855986dfc56deb1a831da
petar-staynov <petar.staynov@gmail.com>
2016-10-05 02:27:18 +0300

    Game Start Fade Out

--- js/StartScreen.js
+++ js/StartScreen.js
@@ -6,15 +6,32 @@ function StartScreen(width, height, x, y) {
     this.img = new Image();
     this.img.src = 'res/StartButton.png';
 
+    var alpha = 1;
+    var ctx = myGameArea.context;
+    ctx.globalAlpha = 1;
+
     this.update = function() {
-        var ctx = myGameArea.context;
         ctx.drawImage(this.img, this.x,this.y, this.width, this.height);
     };
 
     this.onPressEnter = function () {
         if (myGameArea.keys && myGameArea.keys[13] && pressEnterOnce){
-            $("StartButton.png").fadeOut();
-            closedStartMenu = true;
+
+
+            //contex FADE OUT EFFECT ON START
+            var counter = 0;
+            var fade = setInterval(function () {
+
+                ctx.globalAlpha -= 0.020;
+
+                counter++;
+
+                if(counter == 16) {
+                    closedStartMenu = true;
+                    ctx.globalAlpha = 1;
+                    clearInterval(fade);
+                }
+            }, 100);
         }
     }
 }
