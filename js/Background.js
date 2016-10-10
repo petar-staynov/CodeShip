82fea1887008ef884136d2767e8d505451d20589
hristo2612 <hristoapps@gmail.com>
2016-10-10 22:40:58 +0300

    Background optimization

--- js/Background.js
+++ js/Background.js
@@ -6,13 +6,11 @@ function Background(width, height, x, y) {
     this.y2 = y - 638;
     this.img = new Image();
     this.img.src = 'res/background.png';
-    this.img2 = new Image();
-    this.img2.src = 'res/background.png';
 
     this.update = function() {
         var ctx = myGameArea.context;
         ctx.drawImage(this.img, this.x,this.y, this.width, this.height);
-        ctx.drawImage(this.img2, this.x,this.y2, this.width, this.height);
+        ctx.drawImage(this.img, this.x,this.y2, this.width, this.height);
 
     };
 
